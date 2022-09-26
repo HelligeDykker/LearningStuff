78db007f66770980f45cadbc14187963f92d2476
HelligeDykker <114433279+HelligeDykker@users.noreply.github.com>
2022-09-26 20:33:27 +0200

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,55 @@
+﻿//namespace Demo
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (var i = 1; i <= 10; i++)
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
+
+//            for (var i = 10; i >= 1; i--)
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
+//            Console.WriteLine('\n');
+
+//            var numbers = new int[] { 1, 2, 3, 4 };
+
+//            foreach (var number in numbers)
+//            {
+//                Console.WriteLine(number);
+//            }
+//            Console.WriteLine('\n');
+
+//            while (true)
+//            {
+//                Console.Write("type your name: ");
+//                var input = Console.ReadLine();
+
+//                if (!string.IsNullOrWhiteSpace(input))
+//                {
+//                    Console.WriteLine("@Echo: " + input);
+//                    continue;
+//                }
+//                break;
+                       
+//            }
+//        }
+       
+        
+
+
+
+
+
+
+//    }
+//}
